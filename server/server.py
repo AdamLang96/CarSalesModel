abfb2bbe21303bd84bf3b1c0fab51f0d418a0ee5
AdamLang96 <45542095+AdamLang96@users.noreply.github.com>
2023-02-14 11:37:22 -0800

    finished server now pushing to ec2

--- server/server.py
+++ server/server.py
@@ -16,7 +16,6 @@ import pandas as pd
 import boto3
 import pickle as pkl
 import os
-import requests as rq
 
 session = boto3.Session(
     aws_access_key_id = os.environ["ACCESS_KEY"],
@@ -24,6 +23,7 @@ session = boto3.Session(
     region_name = os.environ["REGION"]
 )
 
+s3 = session.resource('s3')
 URI = os.environ["URI"]
 
 engine = create_engine(URI)
@@ -31,6 +31,8 @@ engine = create_engine(URI)
 # scores = pd.read_sql_table('models_score', con=engine)
 # max_score = scores['test_score'].astype(float).idxmax()
 # name = scores["path"][max_score]
+
+mod_api = pkl.loads(s3.Bucket("carsalesmodel").Object('thismod.pkl').get()['Body'].read())
 
 
 
@@ -52,17 +54,9 @@ def predict():
     data = request.get_data()
     data = data.decode('UTF-8')
     data = json.loads(data)
-    data = data['rows'][0]
-    model_name = data['tree_model']
-    del data['tree_model']
-    data = pd.DataFrame(data, index = [0])
-    s3 = session.resource('s3')
-    mod = pkl.loads(s3.Bucket("carsalesmodel").Object(f'{model_name}').get()['Body'].read())
-    preds = mod.predict(data)
+    data = pd.DataFrame(data["rows"], index = [*range(len(data["rows"]))])    
+    preds = mod_api.predict(data)
     return jsonify(list(preds))
-
-if __name__ == '__main__':
-    app.run()
 
 
 
@@ -83,7 +77,6 @@ def predict():
     model_name = data['tree_model']
     del data['tree_model']
     data = pd.DataFrame(data, index = [0])
-    s3 = session.resource('s3')
     mod = pkl.loads(s3.Bucket("carsalesmodel").Object(f'{model_name}').get()['Body'].read())
     preds = mod.predict(data)
     return jsonify(list(preds))
