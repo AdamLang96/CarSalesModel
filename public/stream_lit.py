d82f0b72a5ad4d164373e7cf61b21faf31851241
AdamLang96 <45542095+AdamLang96@users.noreply.github.com>
2023-02-16 15:04:30 -0800

    updated streamlit

--- public/stream_lit.py
+++ public/stream_lit.py
@@ -15,7 +15,7 @@ import os
 import boto3
 from streamlit_option_menu import option_menu
 import altair as alt
-
+import time
 
 session = boto3.Session(
     aws_access_key_id = 'AKIAUH63BSS4PNGLHLFR',
@@ -98,14 +98,14 @@ if selected_navbar == "FAQ":
     with st.container():
         with st.expander("What is Cars and Bids?"):
             st.write('Cars and Bids is an online enthusiast car sales platform created by the automotive Youtuber Doug DeMuro. Most listings on the platform are sold in auction format.')
-        with st.expander("What model are you guys using?"):
-            st.write('Tree based model')
+        with st.expander("What model is being used to predict sale price?"):
+            st.write('We are using a Gradient Boosted Regression Tree to predict sale price')
         with st.expander("How was the data collected?"):
-            st.write('Sold listings are scraped from the website using selenium.')
+            st.write('All of the past listings from CarsAndBids.com were collected using webscraping via selenium. We collected estimated market price for each vehicle from VinAudit.com as well as overall market conditions at the time of sale via Yahoo Finance')
         with st.expander("How accurate are the predictions?"):
-            st.write('MSE of roughly 0.75')
+            st.write('On our best model, we obtain an accuracy of about 75% (MSE .75)')
         with st.expander("Can I use this site commercially?"):
-            st.write('No.')
+            st.write('This site is not intended to be used commercially and should not be used commercially')
         with st.expander("Is the car price prediction sound financial advice?"):
             st.write('No. This is a purely academic exercise; use the model output at your own discretion')
 
@@ -121,74 +121,88 @@ if selected_navbar == "Predict":
         for i, col in enumerate(form_columns):
             if i < 3:
                 for j in range(len(text_arr[i])):
-                    newcol = col.selectbox(text_arr[i][j], options_arr[i][j], key=(i*3)+j)
+                    newcol = col.selectbox(text_arr[i][j], options_arr[i][j], key=(i*3)+j, index=0)
                     columns.append(newcol)
             else:
                 for j in range(len(text_arr[i])):
                     newcol = col.text_input(text_arr[i][j], key=(i*3)+j)
                     columns.append(newcol)
-                newcol = col.selectbox('Model', model_list, key=(i*3)+j+1)
+                newcol = col.selectbox('Model', model_list, key=(i*3)+j+1, index=0)
                 columns.append(newcol)
                 
         sp500 = fetch_market_data()
+        
+        m = st.markdown("""
+            <style>
+            div.stButton > button:first-child {
+                background-color: #0099ff;
+                color:#ffffff;
+            }
+            div.stButton > button:hover {
+                background-color: #00008B;
+                color:#ffffff;
+                } 
+            </style>""", unsafe_allow_html=True)
+        
+        
+        
         button = st.form_submit_button(label="Submit", use_container_width=True)
         
         if button:
-            req = get_vin_info(columns[9])
-            # req = {'mean':60000, 'stdev': 2000, 'count':1000}
-            newres= rq.post(SERVER_URI, json={"rows": [{   "make": columns[0],
-                                                    "model": columns[1],
-                                                    "mileage": columns[10],
-                                                    "status": columns[4], 
-                                                    "engine":columns[3],
-                                                    "drivetrain": columns[5],
-                                                    "transmission" :columns[8],
-                                                    "bodystyle": columns[6],
-                                                    "y_n_reserve":columns[7],
-                                                    "year":columns[2],
-                                                    'market_value_mean': req["mean"], 
-                                                    'market_value_std':req['stdev'], 
-                                                    'count_over_days':str(float(req['count']) / 90), 
-                                                    'Adj Close':sp500,
-                                                    'tree_model': columns[11]}]})
-            response = newres.json()
-            newres = response[0][0]
-            shaps = pd.DataFrame(pd.Series(response[1]))
-            shaps = pd.melt(shaps.reset_index(), id_vars=["index"])
-            print(shaps)
-            chart = (
-                alt.Chart(shaps)
-                .mark_bar()
-                .encode(
-                x=alt.X("value", type="quantitative", title=""),
-                y=alt.Y("index", type="nominal", title=""),
-                color=alt.Color("variable", type="nominal", title=""),
-                order=alt.Order("variable", sort="descending")))
-            st.altair_chart(chart, use_container_width=True)
-            if newres >= float(req["mean"]):
-                SELLSTRING = "recommended"
-            else:
-                SELLSTRING = 'not recommended'
-            st.write(f"Predicted Price on CarsAndBids.com: ${round(newres)}")
-            st.write(f"It is {SELLSTRING} that you sell your car on CarsAndBids.com")
+            try:
+                req = get_vin_info(columns[9])
+                with st.spinner('Running Prediction...'):
+                    time.sleep(5)
+                newres= rq.post(SERVER_URI, json={"rows": [{ "make": columns[0],
+                                                            "model": columns[1],
+                                                            "mileage": columns[10],
+                                                            "status": columns[4], 
+                                                            "engine":columns[3],
+                                                            "drivetrain": columns[5],
+                                                            "transmission" :columns[8],
+                                                            "bodystyle": columns[6],
+                                                            "y_n_reserve":columns[7],
+                                                            "year":columns[2],
+                                                            'market_value_mean': req["mean"], 
+                                                            'market_value_std':req['stdev'], 
+                                                            'count_over_days':str(float(req['count']) / 90), 
+                                                            'Adj Close':sp500,
+                                                            'tree_model': columns[11]}]})            
+                response = newres.json()
+                newres = response[0][0]
+                shaps = pd.DataFrame(pd.Series(response[1]))
+                shaps = pd.melt(shaps.reset_index(), id_vars=["index"])
+                st.subheader('Dollar Contribution of Each Feature to the Predicted Sale Price')
+                chart = (
+                    alt.Chart(shaps)
+                    .mark_bar()
+                    .encode(
+                    x=alt.X("value", type="quantitative", title="Dollars"),
+                    y=alt.Y("index", type="nominal", title="Features"),
+                    color=alt.Color("variable", type="nominal", title="", legend=None),
+                    order=alt.Order("variable", sort="descending")))
+                st.altair_chart(chart, use_container_width=True)
+                st.markdown(f"# Predicted Price on CarsAndBids.com: **${round(newres)}**")
+            except:
+                st.write('Unable to gather information from VIN. Please try a different vehicle')
 
 api_column1, api_column2, api_column3 = st.columns(3)          
 if selected_navbar == "API":
-    st.subheader("Our API is free to use and available at via a POST request to http://collectorcarpricing.com:8080/predict")
+    st.subheader("Our API is free to use and available via a POST request to http://collectorcarpricing.com:8080/predict")
     st.write('The post request must include the following parameters:')
     api_data = { "Name": ['make', 'model', 'mileage', 'status', 'engine', 'bodystyle', 'y_n_reserve','year', 'drivetrain', 'transmission', 'vin'],
                  "Required": ['yes', 'yes', 'yes', 'yes', 'yes', 'yes', 'yes','yes', 'yes', 'yes', 'yes'],
                  "Data Type": ['string', 'string', 'float', 'string', 'string', 'string', 'string','int', 'string', 'string', 'string'],
                  "Accepted Values": ["Any brand of auto manufacturer. If the brand doesnt exist in the training data make will not contribute to the prediction",
-                                    "Any model from an auto manufacturer. If the model doesnt exist in the training data it will use the average value for that make",
+                                    "Any model from an auto manufacturer. If the model doesnt exist in the training data it will use the average price for the chosen make",
                                     "Any positive number (without commas)",
                                     "Clean, Salvage, Other",
-                                    "Any engine. If the engine doesnt exist in the training data it will use the average value for that make",
-                                    "SUV/Crossover, Hatchback, Convertible, Van/Minivan, Sedan, Wagon, Truck, Coupe",
-                                    "Reserve, No Reserve",
-                                    "Any year from 1980-present",
-                                    "Rear-wheel drive, 4WD/AWD, Front-wheel drive",
-                                    "Manual, Automatic",
+                                    "One of the following: (P9, P8, V1, I6, Electric, I2, H6, I3, I5, Flat-2, I4, Flat-4, R6, H4, V6, W8, V2, Flat-6, V8). If not in this list the model will use the average price for the chosen make",
+                                    "One of the following: (SUV/Crossover, Hatchback, Convertible, Van/Minivan, Sedan, Wagon, Truck, Coupe)",
+                                    "One of the following: (Reserve, No Reserve)",
+                                    "Any year from 1980 - present",
+                                    "One of the following: (Rear-wheel drive, 4WD/AWD, Front-wheel drive)",
+                                    "One of the following: (Manual, Automatic)",
                                     "Any valid VIN number"]}
     st.table(pd.DataFrame(api_data))
                                     
