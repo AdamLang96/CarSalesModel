bfa30fc0293957bc47d32b083b9dce7cc38305dc
AdamLang96 <45542095+AdamLang96@users.noreply.github.com>
2023-02-17 09:46:54 -0800

    sorted values for streamlit

---
+++ data/create_data_tables.py
@@ -0,0 +1,43 @@
+from sqlalchemy import Column,  String, create_engine, Integer, text, inspect
+from sqlalchemy.ext.declarative import declarative_base
+Base = declarative_base()
+
+class CarsAndBidsTable(Base):
+    __tablename__ = 'cars_bids_listings'
+    id = Column(Integer(), primary_key=True) 
+    make  = Column(String())
+    model  = Column(String())
+    mileage  = Column(String())
+    vin  = Column(String())
+    status  = Column(String())
+    location = Column(String())
+    engine  = Column(String())
+    drivetrain  = Column(String())
+    transmission  = Column(String())
+    bodystyle = Column(String())
+    exteriorcolor = Column(String())
+    interiorcolor = Column(String())
+    price = Column(String())
+    soldtype = Column(String())
+    numbids = Column(String())
+    y_n_reserve = Column(String())
+    year = Column(String())
+    date = Column(String())
+    url = Column(String())
+    
+class VinAuditTable(Base):
+    __tablename__ = 'vin_audit_data'
+    id = Column(Integer(), primary_key=True)    
+    vin = Column(String())
+    market_value_mean = Column(String())
+    market_value_std = Column(String())
+    count = Column(String())
+    count_over_days = Column(String())
+
+class ModelsScore(Base):
+    __tablename__ = 'models_score'
+    id = Column(Integer(), primary_key=True)    
+    path = Column(String())
+    test_score = Column(String())
+    environment = Column(String())
+
